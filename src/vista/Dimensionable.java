9d08865812fa86692b4cca5676d7e3936d6e1091
Dylan Huilca <dhuilcag@est.ups.edu.ec>
2024-06-15 22:14:58 -0500

    Dimensiones

--- src/vista/Dimensionable.java
+++ src/vista/Dimensionable.java
@@ -10,6 +10,10 @@ public interface Dimensionable {
 	public final int ANCHO = 1100;
 	public final int ALTO = 700;
 	
+	// Dimensiones de la imagen calle1.png
+    public final Dimension IMAGEN_CALLE1_DIMENSION = new Dimension(100, 100); // Dimensiones deseadas de la imagen calle1.png
+    
+	
 	public final int ANCHO_VENTANA = ANCHO + 25 > SCREEN_SIZE.width? SCREEN_SIZE.width : ANCHO + 25;
 	public final int ALTO_VENTANA = ALTO + 50 > SCREEN_SIZE.height? SCREEN_SIZE.height : ALTO + 50;
 	
